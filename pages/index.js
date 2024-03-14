636b035e009836f0d747371806b49e80357873e6
Ryan Shore <mshorecode@gmail.com>
2024-03-14 02:52:45 -0500

    frontend is being crabby

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,33 @@
-import { Button } from 'react-bootstrap';
-import { signOut } from '../utils/auth';
+import React, { useEffect, useState } from 'react';
+import { checkUser } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
+import RegisterForm from '../components/RegisterForm';
 
 function Home() {
   const { user } = useAuth();
+  const [authUser, setAuthUser] = useState({});
+
+  const onUpdate = () => {
+    checkUser(user.uid).then((data) => {
+      setAuthUser(data);
+    });
+  };
+
+  useEffect(() => {
+    checkUser(user.uid).then((data) => {
+      setAuthUser(data).console.log(authUser);
+    });
+  }, []);
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.fbUser.displayName}! </h1>
-      <p>Your Bio: {user.bio}</p>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
-    </div>
+    <>
+      { authUser.uid === user.uid ? (
+        <div className="flex mt-12">
+          <h1 className="font-semibold fs-3">Welcome {user.first_Name}</h1>
+        </div>
+      ) : (<RegisterForm user={user} updateUser={onUpdate} />
+      )}
+    </>
   );
 }
 
