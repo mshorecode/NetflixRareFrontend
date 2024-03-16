1bad4dc71b08e0c90eb06e296bc4bac09cfe09bc
Keana Cobarde <keanacobarde@gmail.com>
2024-03-16 13:12:38 -0500

    writing getReactions API call

---
+++ api/reactionAPI.js
@@ -0,0 +1,19 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getReactions = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/api/reactions`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export default
+{
+  getReactions,
+};
---
+++ components/Reactions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Reactions() {
+  return (
+    <div>Reactions</div>
+  );
+}
+
+export default Reactions;
