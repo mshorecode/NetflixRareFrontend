663923880ffe14ce2f7f9bf200a355cc5266e242
Ryan Shore <mshorecode@gmail.com>
2024-03-16 08:25:45 -0500

    possible fix

--- pages/index.js
+++ pages/index.js
@@ -15,17 +15,18 @@ function Home() {
 
   useEffect(() => {
     checkUser(user.uid).then((data) => {
-      setAuthUser(data).console.log(authUser);
+      setAuthUser(data);
     });
   }, []);
 
   return (
     <>
-      { authUser.uid === user.uid ? (
+      {authUser.uid === user.uid ? (
         <div className="flex mt-12">
           <h1 className="font-semibold fs-3">Welcome {user.first_Name}</h1>
         </div>
-      ) : (<RegisterForm user={user} updateUser={onUpdate} />
+      ) : (
+        <RegisterForm user={user} updateUser={onUpdate} />
       )}
     </>
   );
