9921e4c6453d00f7b016e06d353a6d65b99d50a0
Keana Cobarde <keanacobarde@gmail.com>
2024-03-21 22:41:29 -0500

    writing out general structure for each manager

--- components/forms/TagForm.js
+++ components/forms/TagForm.js
@@ -1,32 +1,32 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 function TagForm() {
   return (
     <form>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Create a Tag</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            This information will be displayed publicly so be careful what you share.
+            Tags are used to categorize content length.
           </p>
-
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
               <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-                Username
+                Tag Name
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
                   <input
                     type="text"
                     name="username"
                     id="username"
                     autoComplete="username"
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                    placeholder="janesmith"
+                    placeholder="Name"
                   />
                 </div>
+                <Button type="submit"> Submit </Button>
               </div>
             </div>
           </div>
--- pages/categorymanager.js
+++ pages/categorymanager.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { Table, Button } from 'react-bootstrap';
 import { getAllCategories } from '../api/categoryApi';
+import CategoryForm from '../components/forms/CategoryForm';
 
 function Categorymanager() {
   const [categories, setCategories] = useState([]);
@@ -15,7 +17,29 @@ function Categorymanager() {
   console.warn(categories);
 
   return (
-    <div>categorymanager</div>
+    <div className="grid grid-cols-2 gap-4 py-4">
+      <div className="display: flex;">
+        <Table className="table-auto">
+          <thead>
+            <tr>
+              <th>Tags</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {categories.map((cat) => (
+              <tr>
+                <td> {cat.label}</td>
+                <td> <Button color="bg-red"> Delete </Button> <Button color="bg-red"> Edit </Button></td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+      <div className="flex">
+        <CategoryForm />
+      </div>
+    </div>
   );
 }
 
